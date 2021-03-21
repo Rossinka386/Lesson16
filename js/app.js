e9b950b7699f860211d2759941ec9c64bfffd865
Rossinka386 <72547965+Rossinka386@users.noreply.github.com>
2021-03-22 00:25:51 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support === true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
+
+
+
+function f11() {
+	document.querySelector('.header__list').classList.toggle('_hidden');
+}
+document.querySelector('.header__burger').addEventListener('click', f11);
